c3a872cfe3fe18f3b45a7693a3a74aa22714a8a9
Juliusz Cieśla <158044000+Doorion@users.noreply.github.com>
2025-06-12 19:18:14 +0200

    Update Player.h

--- include/Player.h
+++ include/Player.h
@@ -1,16 +1,30 @@
 #pragma once
 #include "GameObject.h"
+#include <SFML/Graphics.hpp>
+#include <vector>
 
 class Player : public GameObject {
 public:
     Player(int x, int y);
 
     bool isSolid() const override;
+    std::string getSymbol() const override;
+
     void move(int dx, int dy);
     int getX() const;
     int getY() const;
 
+    void update(float dt) override;
+    void draw(sf::RenderWindow& window) override;
+
 private:
     int x, y;
-};
+    float timeSinceLastFrame = 0.0f;
+    float frameTime = 0.2f;
+    int currentFrame = 0;
 
+    std::vector<sf::Texture> textures;
+    sf::Sprite sprite;
+
+    void loadTextures();
+};
