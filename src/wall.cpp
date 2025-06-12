668734779ed4977b9120445d39bea283693a3557
Juliusz Cieśla <158044000+Doorion@users.noreply.github.com>
2025-06-13 00:38:13 +0200

    Update wall.cpp

--- src/wall.cpp
+++ src/wall.cpp
@@ -1,9 +1,15 @@
 #include "Wall.h"
 
-Wall::Wall() {
-    frames = { "#" }; 
+Wall::Wall(int x, int y) {
+    texture.loadFromFile("assets/Wall.png");
+    sprite.setTexture(texture);
+    sprite.setPosition(static_cast<float>(x * 20), static_cast<float>(y * 20)); // 20 = rozmiar kafelka
 }
 
 bool Wall::isSolid() const {
     return true;
+}
+
+void Wall::draw(sf::RenderWindow& window) const {
+    window.draw(sprite);
 }
