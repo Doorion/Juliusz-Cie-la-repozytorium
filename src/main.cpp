c8e8908b6e47aea19eecc4e8a277966696f20e39
Juliusz Cieśla <158044000+Doorion@users.noreply.github.com>
2025-06-12 14:20:07 +0200

    Update main.cpp
    dodano ruch

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,36 @@
 #include <iostream>
+#include <memory>
+#include <vector>
+#include <chrono>
+#include <thread>
+#include "../include/Player.h"
 
 int main() {
-    std::cout << "Witaj w grze PuzzleGame!" << std::endl;
+    std::shared_ptr<Player> player = std::make_shared<Player>();
+
+    std::cout << "Sterowanie: w (góra), s (dół), a (lewo), d (prawo), q (wyjście)\n";
+
+    char input;
+    bool running = true;
+
+    while (running) {
+        std::cout << "Podaj kierunek: ";
+        std::cin >> input;
+
+        switch (input) {
+            case 'w': player->move(0, -1); break;
+            case 's': player->move(0, 1); break;
+            case 'a': player->move(-1, 0); break;
+            case 'd': player->move(1, 0); break;
+            case 'q': running = false; break;
+            default:
+                std::cout << "Nieznany ruch!\n";
+        }
+
+        player->update(0); // deltaTime = 0 (bo tu nie robimy animacji)
+        std::cout << "Pozycja: (" << player->getX() << ", " << player->getY() << ")\n";
+    }
+
+    std::cout << "Koniec gry.\n";
     return 0;
 }
-
